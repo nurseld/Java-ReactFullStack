49c5b582e9264259919bbe804fbb540e989757ea
nurseld <61293125+nurseld@users.noreply.github.com>
2023-11-01 11:42:17 +0300

    20231101 - Spring Workshop

---
+++ Workshops/workshop4/spring.example/src/main/java/com/tobeto/spring/example/Products.java
@@ -0,0 +1,9 @@
+package com.tobeto.spring.example;
+
+public class Products {
+    public String id;
+    public int price;
+    public String name;
+    public String description;
+    public String categoryName;
+}
---
+++ Workshops/workshop4/spring.example/src/main/java/com/tobeto/spring/example/ProductsController.java
@@ -0,0 +1,24 @@
+package com.tobeto.spring.example;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/products")
+public class ProductsController {
+
+    private List<Products> productsList = new ArrayList<>();
+
+    @GetMapping
+    public List<Products> getAll() {
+        return productsList;
+    }
+
+    @PostMapping
+    public String add(@RequestBody Products products){
+        productsList.add(products);
+        return "Added";
+    }
+}
