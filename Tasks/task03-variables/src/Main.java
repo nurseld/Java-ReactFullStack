96d318f7382b07c4added7f0b17d9d005c808a6b
Nursel Demirkiran <demirkiran3@gmail.com>
2023-12-13 00:01:22 +0300

    Task variables addded.

---
+++ Tasks/task03-variables/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numberOfStudent = 12;
+        String message = "Number of students : ";
+        System.out.println(message + numberOfStudent);
+        System.out.println(message + numberOfStudent);
+        System.out.println("Öğrenci Sayısı : " + numberOfStudent);
+        System.out.println("Öğrenci Sayısı : " + numberOfStudent);
+        System.out.println("Öğrenci Sayısı : " + numberOfStudent);
+    }
+}
