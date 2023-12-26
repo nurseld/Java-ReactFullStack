1cbf0726e66c4ab3b032674773d6d6e763f74ac2
Nursel Demirkiran <demirkiran3@gmail.com>
2023-12-26 20:09:21 +0300

    Initialize React project

--- Workshops/ReactWorkshops/Workshop1/src/index.js
+++ Workshops/ReactWorkshops/Workshop1/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// self closing tag => <img />
+// normal tag => <a> </a>
+root.render(<App></App>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
