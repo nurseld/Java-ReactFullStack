423f14cfb181cd0426c03087dbda910b48985e80
Nursel Demirkiran <demirkiran3@gmail.com>
2024-03-04 20:23:04 +0300

    Task dataTypes added

---
+++ Tasks/task04-dataTypes/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //byte,short,long
+
+        int number = 28;
+
+        double number2 = 12.5;
+        number2 = -129;
+
+        char character = 'A';
+
+        boolean isTrue = false;
+
+    }
+}
